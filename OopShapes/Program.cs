c0bb2febc714acf3ed426357e76d34a47c1562ca
Edita Liutkeviciene <edita.rinkunaite@gmail.com>
2023-04-13 19:35:56 +0300

    Add project files.

---
+++ OopShapes/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OopShapes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square1 = new Square(5);
+            Shape square2 = new Square(2);
+
+            Console.WriteLine($"Area of the square: {square1.GetArea()}");
+            Console.WriteLine($"Area of the square: {square2.GetArea()}
+        }
+    }
+}
---
+++ OopShapes/Squares.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopShapes
+{
+    internal class Squares : Shape
+    {
+        private int side;
+
+        public Squares(int a)
+        {
+            this.side = a;
+        }
+
+        public override int GetArea()
+        {
+            return this.side = this.side
+        }
+    }
+}
